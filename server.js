37f0ccdb1f7ab8e2a9ea6961acf495e719963289
adaruiz <ada.ruiz93@gmail.com>
2021-09-30 10:00:29 -0700

    cors

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 require('./db/mongoose')
 const PORT = process.env.PORT || 8080
 const blistController = require('./controllers/blist')
+const urlArray = ['http://localhost:8080']
+const corsOption = {
+  origin: (origin, cb) => {
 
+    if(urlArray.indexOf(origin) != -1)
+  }
+}
 app.use(express.json())
 app.use('/blist', blistController)
 
